bc98f5df456abb1d21bb134751a33073f8057214
marvanessa1 <marvanessa1@live.com>
2021-12-17 22:52:59 -0800

    added time blocks and added timeblock funtion so that each timeblock is color coded to indicate if its past, present, or future

--- script.js
+++ script.js
@@ -2,3 +2,25 @@ var Container = $('container');
 
 var currentDay = moment();
     $('#currentDay').text(currentDay.format("MMMM Do YYYY"));
+
+for (var i=8; i<24; i++){
+
+    var hourValue = localStorage.getItem("hr-" + i);
+
+    var TimeBlock = $("#hr-" + i)
+    if (i <moment().hour()){
+        TimeBlock.addClass("past")
+    }
+    if (i == moment().hour()){
+        TimeBlock.addClass("present")
+    }
+    if (i>moment().hour()){
+        TimeBlock.addClass("future")
+    }
+}
+
+// var ButtonSaveEl = $("button");
+
+// ButtonSaveEl.click(function(event){
+//     var 
+// });
